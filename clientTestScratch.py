ac0d6c3e2521f1975ef9a28176f25f2eb1ad94c4
rosstimo <rosstimo@isu.edu>
2019-05-15 12:55:52 +0100

    a little progress on sniffer

--- clientTestScratch.py
+++ clientTestScratch.py
@@ -6,16 +6,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 42001
 s.connect((host, port))
-
+full_msg = ''
 while True:
-    full_msg = ''
-    while True:
-        msg = s.recv(1024)
-        if len(msg) <= 0:
-            break
-        full_msg += msg.decode("utf-8")
-
-    if len(full_msg) > 0:
-        print(full_msg)
-    #else:
-        #s.close()
+    msg = s.recv(1024)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
+if len(full_msg) > 0:
+    print(full_msg)
+s.close()
