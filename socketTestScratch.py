38f210927a3522940ddd8f983a6724a92a6a1340
rosstimo <rosstimo@isu.edu>
2019-05-17 16:11:52 +0100

    a little progress on sniffer

--- socketTestScratch.py
+++ socketTestScratch.py
@@ -10,7 +10,7 @@ except socket.error as err:
     print "socket creation failed with error %s" %(err)
 
 # default port for socket
-port = 42001
+port = 42002
 
 try:
     host_ip = socket.gethostbyname('127.0.0.1')
@@ -23,6 +23,11 @@ except socket.gaierror:
 
 # connecting to the server
 s.connect((host_ip, port))
+dummy = 'sensor-update "dummy" 77'
+n = len(dummy)
+b = (chr((n >> 24) & 0xFF)) + (chr((n >> 16) & 0xFF)) + (chr((n >> 8) & 0xFF)) + (chr(n & 0xFF))
+print str(n) + dummy
+s.send(b + dummy)
 
 print "the socket has successfully connected to scratch \
 on port == %s" %(host_ip)
