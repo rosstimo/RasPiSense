b70c71f301718134cf35909d61a580e7a3c55651
rosstimo <rosstimo@isu.edu>
2019-06-06 13:31:42 +0100

    scratch sensor update class working

--- SensorUpdateScratch.py
+++ SensorUpdateScratch.py
@@ -2,51 +2,57 @@
 import socket 
 import sys
 
-def sendUpdate(SensorUpdateMessage, host_ip, port):
-        try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                print('Socket successfully created')
-        except socket.error as err:
-                print('socket creation failed with error %s" %(err)')
-
-                # default port for socket
-                port = 42001
-
-        try:
-                host_ip = socket.gethostbyname(socket.gethostname())
-                print('the host name is: ' + host_ip)
-                print('the port is: ' + str(port))
-                # connecting to the server
-                s.connect((host_ip, port))
-        #dummy = 'sensor-update "dummy" 99'
-                #the next two line pad the begining of dummy with 3 byte of 0x00h
-                #n = len(dummy)
-                #b = (chr((n >> 24) & 0xFF)) + (chr((n >> 16) & 0xFF)) + (chr((n >> 8) & 0xFF)) + (chr(n & 0xFF))
-                #print('sending: ' + str(n) + dummy)
-                s.send(b + dummy)
-                print "the socket has successfully connected to scratch on port == %s" %(host_ip)
-                s.close()
-                
-        except socket.gaierror:
-                # this means could not resolve the host
-                print "there was an error resolving the host"
-                sys.exit()      
-
-        except socket.error as err:
-                print('it looks like the server doesn\'t want to talk')
-                print(err)
-                #break
-         
-def SensorUpdateMessage(sensorName, SensorValue):               
-        concat_message = 'sensor-update "' + dummy + '"' + str(99)
-        #the next two line pad the begining of dummy with 3 byte of 0x00h
-        n = len(concat_message)
-        paddedMessage = (chr((n >> 24) & 0xFF)) + (chr((n >> 16) & 0xFF)) + (chr((n >> 8) & 0xFF)) + (chr(n & 0xFF))
-        print('sending: ' + str(n) + concat_message)      
+class ScratchSensorUpdate():
         
-SensorName = "mySensor"
-SensorValue = 42
+        def __init__(self, host_ip, port, sensorName, sensorValue):
+                self.host_ip = host_ip
+                self.port = port
+                self.sensorName = sensorName
+                self.sensorValue = sensorValue
+                
+        def sendUpdate(self):
+                try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        print('Socket successfully created')
+                except socket.error as err:
+                        print('socket creation failed with error %s" %(err)')
 
+                try:
+                        if True: #TODO test if ip and port should be set to defaults
+                                host_ip = socket.gethostbyname(socket.gethostname())
+                                port = 42001
+                        print('the host name is: ' + self.host_ip)
+                        print('the port is: ' + str(self.port))
+                        # connecting to the server
+                        s.connect((self.host_ip, self.port))
+                        s.send(self.sensorMessage())
+                        print "the socket has successfully connected to scratch on port == %s" %(host_ip)
+                        s.close()
+                        
+                except socket.gaierror:
+                        # this means could not resolve the host
+                        print "there was an error resolving the host"
+                        sys.exit()      
 
-    
+                except socket.error as err:
+                        print('it looks like the server doesn\'t want to talk')
+                        print(err)
+                        #break
+         
+        def sensorMessage(self):               
+                concat_message = 'sensor-update "' + self.sensorName + '" ' + str(self.sensorValue)
+                #the next two line pad the begining of dummy with 3 byte of 0x00h
+                n = len(concat_message)
+                pad = (chr((n >> 24) & 0xFF)) + (chr((n >> 16) & 0xFF)) + (chr((n >> 8) & 0xFF)) + (chr(n & 0xFF))
+                print('sending: ' + str(pad) + concat_message)      
+                return(pad + concat_message)
+     
+if __name__ == '__main__' :
+        sensorName = "mySensor"
+        sensorValue = 42
+        host_ip = '127.0.0.1'
+        port = 42001
+        testUpdate = ScratchSensorUpdate(host_ip, port, sensorName, sensorValue)
+        print(testUpdate.port)
+        testUpdate.sendUpdate()
 
--- dummy.py
+++ dummy.py
@@ -33,24 +33,6 @@ class dummy():
 
 
 
-	def sender(self):
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((socket.gethostname(), 1234))
-		except
-			print('somethng went wrong')
-
-
-		while True:
-		    full_msg = ''
-		    while True:
-		        msg = s.recv(8)
-		        if len(msg) <= 0:
-		            break
-		        full_msg += msg.decode("utf-8")
-
-		    if len(full_msg) > 0:
-		        print(full_msg)
 
 
 
